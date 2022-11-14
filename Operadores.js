20870d38535dad49635c7130881e4899443d43ce
David Marins <davidmarinsdiogo01@gmail.com>
2022-11-13 22:48:23 -0300

    Operadores aritmédicos

---
+++ Operadores.js
@@ -0,0 +1,22 @@
+
+
+let numberOne = parseInt(prompt ("Digite um número:"))
+let numberTwo = parseInt( prompt ("Digite um número:"))
+
+ let soma  = numberOne + numberTwo
+ let sub = numberOne - numberTwo
+
+console.log("Soma dois números :",soma)
+console.log("Subtração dois números:",sub)
+
+let div = 80
+let div2 = 3
+
+let resto = div / div2
+console.log("Resto da divisão 80/3 :",resto)
+
+console.log(soma + 5)
+console.log(soma - 3)
+
+
+
